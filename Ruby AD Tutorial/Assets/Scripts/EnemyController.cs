077cd89327b4d3fd0994bdc136cbcf321c1de1b1
alfp12 <53943350+alfp12@users.noreply.github.com>
2020-01-16 19:58:34 -0300

    Enemy dev.

---
+++ Ruby AD Tutorial/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed = 3.0f;
+    public bool vertical;
+    public float changeTime = 3.0f;
+
+    Rigidbody2D rigidbody2D;
+    float timer;
+    int direction = 1;
+
+    void Start() {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        timer = changeTime;
+    }
+
+    void Update() {
+        timer -= Time.deltaTime;
+
+        if (timer < 0)
+        {
+            direction = -direction;
+            timer = changeTime;
+        }
+
+
+        Vector2 position = rigidbody2D.position;
+
+        if (vertical)
+        {
+            position.y = position.y + Time.deltaTime * speed;
+        }
+        else
+        {
+            position.x = position.x + Time.deltaTime * speed;
+        }
+        
+        rigidbody2D.MovePosition(position);
+    }
+}
--- Ruby AD Tutorial/Assets/Scripts/RubyController.cs
+++ Ruby AD Tutorial/Assets/Scripts/RubyController.cs
@@ -7,7 +7,13 @@ public class RubyController : MonoBehaviour
     public float speed = 3.0f;
 
     public int maxHealth = 5;
+
+    public int health { get {return currentHealth; }}
     int currentHealth;
+
+    public float timeInvincible = 2.0f;
+    bool isInvincible;
+    float invincibleTimer;
 
     Rigidbody2D rigidbody2D;
     // Start is called before the first frame update
@@ -17,13 +23,13 @@ public class RubyController : MonoBehaviour
 
            currentHealth = maxHealth;
 
-           currentHealth = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
+
         float vertical = Input.GetAxis("Vertical");
 
         Vector2 position = rigidbody2D.position;
@@ -32,9 +38,23 @@ public class RubyController : MonoBehaviour
 
         rigidbody2D.MovePosition(position);
 
+        if (isInvincible); {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+            isInvincible = false;
+        }
+
     }
     public void ChangeHealth(int amount)
     {
+        if (amount < 0);
+        {
+            if (isInvincible)
+            return;
+
+            isInvincible =true;
+            invincibleTimer = timeInvincible;
+        }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
